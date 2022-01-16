c4b50a6e86dac015261b268836e533de222e21f0
Vincent Granville <vincentg@datashaping.com>
2022-01-16 01:13:02 -0800

    Produces nearest neighbor graph (image)

--- PB_NN_arrows.r
+++ PB_NN_arrows.r
@@ -1,23 +1,18 @@
-# Produces nearest neighbor graphs, with arrow point from any point to its nearest neighbor.
-# Assumes we are dealing with a superimposition (or mixture) of 5 point processes.
-# Each point is assigned the color of the individual point process it belongs to.
-# Only a small window is displayed: x in [0, 5.75] and y in [0.5.75]
-#
-# Input file: PB_r.txt, produced by PB_NN.pl
-# Output: image dispayed on R console (when using RStudio)
+# install.packages('Cairo')
+library('Cairo');
+# CairoWin(6,6);
+CairoPNG(filename = "c:/Users/vince/tex/PB-hexa2.png", width = 600, height = 600);
 
 data<-read.table("c:/Users/vince/tex/PB_r.txt",header=TRUE);
-
 a<-data$a;  # x coordinate of point of the superimposed/mixture process 
 b<-data$b;  # y coordinate of point of the superimposed/mixture process 
-aNN<-data$aNN;  # x coordinate of nearest neighbor point to (a,b) among all processes
-bNN<-data$bNN;  # y coordinate of nearest neighbor point to (a,b) among all processes
+aNN<-data$aNN;  # x coordinate of nearest neighbor point to (a,b) across all processes
+bNN<-data$bNN;  # y coordinate of nearest neighbor point to (a,b) across all processes
 processID<-data$processID;
 
-plot(a,b,xlim=c(0,5.75),ylim=c(0,5.75),pch=20,cex=0,col=rgb(0,0,0),xlab="",ylab="",axes=TRUE  );
-arrows(a, b, aNN, bNN, length = 0.10, angle = 10, code = 2,col=rgb(0,0,0));
-# reference: https://stat.ethz.ch/R-manual/R-devel/library/graphics/html/arrows.html
-
+plot(a,b,xlim=c(0,15),ylim=c(0,15),pch=20,cex=0,
+                    col=rgb(0,0,0),xlab="",ylab="",axes=TRUE  );
+arrows(a, b, aNN, bNN, length = 0.10, angle = 10, code = 2,col=rgb(0.7,0.7,0.7));
 
 aa<-data$a[processID == 0];
 bb<-data$b[processID == 0];
@@ -39,3 +34,4 @@ aa<-data$a[processID == 4];
 bb<-data$b[processID == 4];
 points(aa,bb,col=rgb(0,0.7,0),pch=20,cex=1.75);
 
+dev.off();
